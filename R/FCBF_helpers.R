5f814d2439dce322ef658a05d02e4c1a9f85c202
Rowan Jacques-Hamilton <rowan.jacques.hamilton@gmail.com>
2022-02-26 11:43:25 +0100

    Added cutpoint, restructured scripts

--- R/FCBF_helpers.R
+++ R/FCBF_helpers.R
@@ -1,19 +1,25 @@
 # Helper functions
-# discretize_var <- function(vector, quantile){
-#     ### Helper function for FCBF.
-#     if(!is.numeric(vector)){stop("Feature must be numeric to discretize")}
-#     results <- rep('l', length(vector))
-#     results[vector > median(vector)] <- 'h'
-#     return(results)
-# }
+discretize_var <- function(vector, cutpoint){
+    ### Helper function for FCBF.
+    if(!is.numeric(vector)){
+        rlang::abort("Feature must be numeric to discretize")
+    }
+    if(cutpoint <=0 | cutpoint >=1){
+        rlang::abort("cutpoint must be between 0 and 1")
+    }
+    results <- rep('l', length(vector))
+    cut <- quantile(vector, cutpoint)
+    results[vector > cut] <- 'h'
+    return(results)
+}
 
-FCBF_helper <- function(preds, outcome, min_su, n_cuts){
+FCBF_helper <- function(preds, outcome, min_su, cutpoint){
     ### Takes a set of predictors, does FCBF for feature selection, and
     ### returns the names of the features to keep.
 
     preds <- preds %>%
-        purrr::map_if(is.numeric, ~recipes::discretize(.x, n_cuts))
-
+        purrr::map_if(is.numeric, ~discretize_var(.x, cutpoint = cutpoint)) %>%
+        as_tibble()
     res <- FCBF::fcbf(feature_table = preds, target_vector = outcome, minimum_su = min_su,
                 verbose = FALSE, samples_in_rows = TRUE)
     return(res)
--- R/step_FCBF.R
+++ R/step_FCBF.R
@@ -27,23 +27,109 @@
 #' @examples test
 #'
 #' @export
-step_FCBF <- function (recipe, ..., min_su = 0.025, outcome = NA, n_cuts = 2,
+step_FCBF <- function (recipe, ..., min_su = 0.025, outcome = NA, cutpoint = 0.5,
                        features_retained = NA, role = NA, trained = FALSE,
                        removals = NULL, skip = FALSE, id = rand_id("FCBF")) {
+    # Check arguments
+
+    recipes::recipes_pkg_check(required_pkgs.step_FCBF())
 
     add_step(recipe, step_FCBF_new(terms = enquos(...), min_su = min_su,
-                                   outcome = outcome, n_cuts = n_cuts,
+                                   outcome = outcome, cutpoint = cutpoint,
                                    features_retained = features_retained,
                                    role = role, trained = trained,
                                    removals = removals, skip = skip, id = id))
 }
 
 # Constructor (boilerplate)
-step_FCBF_new <- function (terms, min_su, outcome, n_cuts, features_retained,
+step_FCBF_new <- function (terms, min_su, outcome, cutpoint, features_retained,
                            role, trained, removals, skip, id) {
     step(subclass = "FCBF", terms = terms, min_su = min_su, outcome = outcome,
-         n_cuts = n_cuts, features_retained = features_retained,
+         cutpoint = cutpoint, features_retained = features_retained,
          role = role, trained = trained, removals = removals, skip = skip, id = id)
 }
 
+#' @importFrom recipes prep
+#' @export
+#'
+prep.step_FCBF <- function (x, training, info = NULL, ...){
+    # Find outcome column
+    if(!is.na(x$outcome)){
+        if(class(x$outcome) != "character"){
+            rlang::abort("Outcome variable for step_FCBF must be supplied as a character string")
+        }
+        if(length(x$outcome) > 1){
+            rlang::abort("Only a single outcome variable can be supplied for step_FCBF")
+        }
+        outcome_col <- x$outcome
+    } else{
+        outcome_col <- info %>% dplyr::filter(role == 'outcome') %>% dplyr::pull(variable)
+        if(length(outcome_col)>1){
+            rlang::abort(paste0("step_FCBF found more than one outcome variable.",
+                                "Only a single outcome variable can be accepted by FCBF. Please",
+                                "supply the outcome variable using the outcome argument in step_FCBF"))
+        }
+    }
+    if(length(outcome_col)<1|is.na(outcome_col)){
+        rlang::abort(paste("An outcome varaible was not found by step_FCBF. Please",
+                           "ensure an outcome variable is specified."))
+    }
+    # Get predictor columns to be used in FCBF
+    pred_cols <- recipes_eval_select(x$terms, training, info)
 
+    if (length(pred_cols) <= 1) {
+        # this message is not given by FCBF::fcbf when only 1 predictor is selected by the filter
+        rlang::warn("Fewer than two predictors were supplied to step_FCBF, FCBF will not be conducted")
+    }
+    fcbf_out <- FCBF_helper(preds = training[, pred_cols],
+                            outcome = training[, outcome_col, drop = TRUE],
+                            min_su = x$min_su,
+                            cutpoint = x$cutpoint)
+    cols_selected <- pred_cols[fcbf_out$index]
+    if(length(cols_selected) == 1) print("Number of features selected =  1")
+
+    # Specify which cols to remove from the training set
+    remove_cols <- pred_cols[!pred_cols %in% cols_selected]
+
+    # Keep a record of which predictors were retained (potentially useful after fitting resamples etc.)
+    vars_retained <-
+        info %>% dplyr::filter(role == 'predictor', !variable %in% remove_cols)
+
+    # keep_cols <- c(cols_selected, outcome_col)
+    step_FCBF_new(terms = x$terms, min_su = x$min_su, outcome = x$outcome,
+                  cutpoint = x$cutpoint,
+                  features_retained = vars_retained, role = x$role, trained = TRUE,
+                  removals = remove_cols, skip = x$skip, id = x$id)
+}
+
+#' @importFrom recipes bake
+#' @export
+bake.step_FCBF <- function (object, new_data, ...) {
+    if (length(object$removals) > 0)
+        new_data <- new_data[, !colnames(new_data) %in% object$removals]
+    as_tibble(new_data)
+
+}
+
+#' @export
+print.step_FCBF <- function (x, width = max(20, options()$width - 36), ...){
+    title <- "FCBF retained : "
+    print_step(x$features_retained$variable, x$terms, x$trained, title, width)
+    title <- "FCBF removed: "
+    print_step(x$removals, x$terms, x$trained, title, width)
+
+    invisible(x)
+}
+
+#' S3 methods for tracking which additional packages are needed for steps.
+#'
+#' Recipe-adjacent packages always list themselves as a required package so that
+#' the steps can function properly within parallel processing schemes.
+#' @param x A recipe step
+#' @return A character vector
+#' @rdname required_pkgs.embed
+#' @keywords internal
+#' @export
+required_pkgs.step_FCBF <- function(x, ...) {
+    c("FCBF")
+}
